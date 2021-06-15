d48f011cf693db2ed237937123a95ef0b05502f3
Мещеряков Вадим <v.meshcheryakov@k-telecom.org>
2021-06-15 19:10:29 +0500

    Написал метод, который принимает схему и таблицу, а возвращает множество грантов.

--- run.py
+++ run.py
@@ -1,47 +1,57 @@
+import enum
+import re
+from collections import namedtuple
+# from pprint import pprint
+
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import Session
-import re
-import enum
-from dotenv import load_dotenv, find_dotenv
-import os
-from grants.utils import get_engine
 
-load_dotenv(find_dotenv('.env', raise_error_if_not_found=True))
-root_engine = get_engine('root', os.getenv('DB_PASS'))
-user_engine = get_engine(os.getenv('DB_USER'), os.getenv('DB_PASS'))
+from config import user_engine
 
-
-# Еще надо обработать revoke
-RE_GRANT = re.compile(r'GRANT (.*) ON (.*)\.(.*) TO (.*)@\'(.*)\'')
+RE_GRANT = re.compile(r'GRANT (?P<privileges>.*) ON (?P<schema>\S*)\.(?P<table>\S*)')
 
 
 class GrantLevel(enum.Enum):
-    GLOBAL = 0
-    SCHEMA = 1
-    TABLE = 2
+    GLOBAL = enum.auto()
+    SCHEMA = enum.auto()
+    TABLE = enum.auto()
+
+
+class Privileges(enum.Enum):
+    ALL = 'ALL PRIVILEGES'
+    DELETE = 'DELETE'
+    INSERT = 'INSERT'
+    SELECT = 'SELECT'
+    UPDATE = 'UPDATE'
+    USAGE = 'USAGE'
+
+
+Grants = namedtuple('Grants', ['privileges', 'schema', 'table'])
 
 
 class MySQLGrant:
-    def __init__(self, engine: Engine, username: str = ''):
+    def __init__(self, engine: Engine, username: str = '', host: str = '%'):
         self._engine = engine
         self._username = username or engine.url.username
-        self._grants = dict.fromkeys(GrantLevel.__members__)
+        self._host = host
+        self._grants = {key: [] for key in GrantLevel}
         self._update_grants()
 
-    def _update_grants(self):
+    def _get_grants(self) -> list:
         try:
             with Session(self._engine) as session:
                 query = session.execute(f'SHOW GRANTS FOR {self._username}')
-                result = query.all()
+                return query.all()
         except Exception as e:
             raise e
 
-        for grant in result:
+    def _update_grants(self) -> None:
+        for grant in self._get_grants():
             match = RE_GRANT.match(grant[0])
             if match:
-                privileges = [p.strip() for p in match.group(1).split(',')]
-                schema = match.group(2).strip('`')
-                table = match.group(3).strip('`')
+                privileges = [p.strip() for p in match.group('privileges').split(',')]
+                schema = match.group('schema').strip('`')
+                table = match.group('table').strip('`')
 
                 if table != '*':
                     level = GrantLevel.TABLE
@@ -50,20 +60,42 @@ class MySQLGrant:
                 else:
                     level = GrantLevel.GLOBAL
 
-                print('Privileges:', privileges)
-                print('Schema:', schema)
-                print('Table:', table)
-                print('Level:', level)
+                grants = Grants(privileges, schema, table)
+                self._grants[level].append(grants)
 
-            print()
+    def check_permissions(self, schema: str = '*', table: str = '*') -> set:
+        result = set()
+        grant: Grants
+        for grant in self._grants[GrantLevel.TABLE]:
+            if grant.schema in ('*', schema) and grant.table in ('*', table):
+                set.update(result, grant.privileges)
+
+        for grant in self._grants[GrantLevel.SCHEMA]:
+            if grant.schema in ('*', schema) and grant.table in ('*', table):
+                set.update(result, grant.privileges)
+
+        for grant in self._grants[GrantLevel.GLOBAL]:
+            if grant.schema in ('*', schema) and grant.table in ('*', table):
+                set.update(result, grant.privileges)
+
+        if 'ALL PRIVILEGES' in result:
+            return {'ALL PRIVILEGES'}
+
+        if len(result) > 1:
+            result.discard('USAGE')
+
+        return result
 
 
 def main():
-    print('='*10, 'ROOT', '='*10)
-    MySQLGrant(root_engine)
+    grants = MySQLGrant(user_engine)
+    # pprint(grants._grants)
 
-    print('='*10, 'USER', '='*10)
-    MySQLGrant(user_engine)
+    print()
+    print('Grants on test.*', grants.check_permissions('test'), sep='\t')
+    print('Grants on test.test_table', grants.check_permissions('test', 'test_table'), sep='\t')
+    print('Grants on test2.*', grants.check_permissions('test2'), sep='\t')
+    print('Grants on test2.test_table', grants.check_permissions('test2', 'test_table'), sep='\t')
 
 
 if __name__ == "__main__":
