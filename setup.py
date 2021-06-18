06f54195b0a94fc922b2314a29f09beb18750b9a
Мещеряков Вадим <v.meshcheryakov@k-telecom.org>
2021-06-18 16:23:01 +0500

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-SQLGrants',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     description='Grants processing library',
     author='Vadim Meshcheryakov',
